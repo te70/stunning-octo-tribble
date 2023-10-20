b6ce2e59507822189ee24c6ca30d2006ea56bd99
te70 <tevinkinuthia@gmail.com>
2023-10-20 20:50:57 +0400

    Add controller functions

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -20,4 +20,26 @@ func init(){
 	db.AutoMigrate(&book{})
 }
 
- 
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book(
+	var Books []Book
+	db.Find(&Books)
+	return Books
+)
+
+func GetBookById(Id int64) (*Book, *gorm.DB){
+	var getBook book
+	db:=db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book(
+	var book Book
+	db.where("ID=?", ID).Delete(book)
+	return book
+)
